9fced844d12d6fe37c827cd589fb4dd5e1aefdcd
Melnikov Aleksey <shvejk@bk.ru>
2017-06-29 15:37:41 +0300

    Adds wide medias enhancements

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 "use strict"
 
+        /*      toggle navigation
+         ********************************/
+
 var navToggleButton = document.querySelector('.js-nav-toggle')
 var navBar = document.querySelector('.js-companies-nav')
 
@@ -12,4 +15,25 @@ navToggleButton.addEventListener("click", function(e) {
 document.body.addEventListener("click", function() {
     navBar.classList.remove("u-show")
     navBar.classList.remove("u-opaque")
+})
+
+
+
+
+        /*      toggle contacts
+         *********************************/
+
+var contacts = document.querySelector('.js-contacts')
+var contactsToggleButton = document.querySelector('.js-minimize-contacts')
+var symbol = document.querySelector('.js-minimize-symbol')
+
+var symbolUp = document.createTextNode("\uf106")
+var symbolDown = document.createTextNode("\uf107")
+
+symbol.appendChild(symbolUp)
+
+contactsToggleButton.addEventListener("click", function(e) {
+    contacts.classList.toggle("u-remove")
+    if (symbol.firstChild == symbolUp) symbol.replaceChild(symbolDown, symbolUp)
+    else symbol.replaceChild(symbolUp, symbolDown)
 })
